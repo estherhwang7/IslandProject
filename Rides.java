c66d44e7b9b089d13104bab57abcfd86021d241d
calebalexandre482 <144266036+calebalexandre482@users.noreply.github.com>
2024-02-09 12:56:04 -0500

    rides class3
    rides class

--- Rides.java
+++ Rides.java
@@ -3,15 +3,21 @@ public class Rides {
     //Instance variables
     private String name;
     private String color;
-    private int customers;
+    private int people;
     private boolean open;
 
     //Constructors
-    public Rides(String n, int s, String c) {
+    public Rides(String n, String c) {
         name = n;
         color = c;
         open = true;
-        customers = 0;
+        people = 0;
+    }
+    public Rides(String n, String c, int p) {
+        name = n;
+        color = c;
+        open = true;
+        people = p;
     }
 
     //Get methods
@@ -21,9 +27,10 @@ public class Rides {
     public String getColor() {
         return color;
     }
-    public boolean getOpen() {
-        return open;
+    public int getPeople() {
+        return people;
     }
+    
 
     //Set methods
     public void setName(String a) {
@@ -34,20 +41,28 @@ public class Rides {
     }
     
     //Other variables
-    public void shutDown() {            //the ride being shut down
+    public void shutDown() {            //The ride is being shut down
         open = false;
     }
-    public void ride(int n) {           //people going on the ride
+    public void ride(int n) {           //Adds to the total number of people that have been on the ride
         if(open) {
-            customers += n;
+            people += n;
+        }
+    }
+    public void isOpen() {
+        if(open) {
+            System.out.println("This ride is open");
+        }
+        else {
+            System.out.println("Thi ride as been shut down");
         }
     }
 
     public String toString() {
         if (open) {
-            return "The name of this " + color + " ride is " + name + "and " + customers + " people have gone on the ride. It is open.";
+            return "The name of this " + color + " ride is " + name + "and " + people + " people have gone on the ride. It is open.";
         }
-        return "The name of this " + color + " ride is " + name + " and " + customers + " people have gone on the ride. It is shut down.";
+        return "The name of this " + color + " ride is " + name + " and " + people + " people have gone on the ride. It is shut down.";
     }
     
 }
